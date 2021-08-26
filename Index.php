dfcb1c5400e5ad25120885d8ffb4a2af8246b507
leomsant <89488875+leomsant@users.noreply.github.com>
2021-08-26 09:01:36 -0300

    Upload novas imagens
    Novas imagens do projeto

---
+++ Index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="shortcut icon" href="images/mochi.png" />    
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Mapa.css">
+    <title>Atividade Mapa - Back End 1</title>
+</head>
