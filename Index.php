f5923dfa1331b188ce77ba9e283d628f2db0f0df
leomsant <89488875+leomsant@users.noreply.github.com>
2021-08-26 21:08:17 -0300

    Criação dos estilos
    Criação dos estilos e formatação dos menus.
    Adição dos estilos no arquivo Estilo.css

--- Index.php
+++ Index.php
@@ -1,15 +1,34 @@
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
-    <link rel="shortcut icon" href="images/mochi.png" />    
     <meta charset="UTF-8">
+    <link rel="shortcut icon" href="images/mochi.png" />    
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="Mapa.css">
-    <title>Atividade Mapa - Back End 1</title>
-    <?php
-	include('pages/Cabecalho.php');
-	include('pages/Rodape.php');    
-?>    
+    <title>Mochi Box</title>
+    <link rel="stylesheet" href="css/Estilo.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500&display=swap" rel="stylesheet">    
 </head>
+
+<body>
+    <header>
+        <img src="images/logo.png" height="100" alt="Mochi Box" title="Mochi Box" class="logo">
+    </header>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Quem somos</li>
+            <li>Contato</li>
+            <li>Localização</li>
+        </ul>
+    </nav>
+    <div class="rdp_content">
+        Conteúdo
+    </div>
+    <footer>
+        Dados acadêmicos
+    </footer>
+</body>
--- Pages/Cabecalho.php
+++
@@ -1,6 +0,0 @@
-<nav>
-    <img src="Images/Logo.png" alt="Instagram" title="Instagram" height="160" width="240">       
-    <h1>
-    Mochi Box
-    </h1>
-</nav>
