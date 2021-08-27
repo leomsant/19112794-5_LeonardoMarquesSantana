9a7316cb96a0f67ee4aac7f4942abbc20ac554e5
leomsant <89488875+leomsant@users.noreply.github.com>
2021-08-27 20:43:46 -0300

    Estrutura page home
    Estruturando a page home dos produtos.

--- Index.php
+++ Index.php
@@ -3,14 +3,14 @@
 
 <head>
     <meta charset="UTF-8">
-    <link rel="shortcut icon" href="images/mochi.png" />    
+    <link rel="shortcut icon" href="images/mochi.png" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mochi Box</title>
     <link rel="stylesheet" href="css/Estilo.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500&display=swap" rel="stylesheet">    
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -25,8 +25,12 @@
             <li>Localização</li>
         </ul>
     </nav>
-    <div class="rdp_content">
-        Conteúdo
+    <div class="cls_prdcontent">
+        <div class="cls_prdcontainer">
+            <?php
+            include('pages/home.php')
+            ?>
+        </div>
     </div>
     <footer>
         Dados acadêmicos
---
+++ Pages/home.php
@@ -0,0 +1,20 @@
+<div class="homeproduto1">
+<img src="images/mochi_belga.jpg" alt="Mochi">
+<h3>Mochi Belga - R$ 5,90</h3>
+</div>
+<div class="homeproduto1">
+<img src="images/mochi_belga.jpg" alt="Mochi">
+<h3>Mochi Belga - R$ 5,90</h3>
+</div>
+<div class="homeproduto1">
+<img src="images/mochi_belga.jpg" alt="Mochi">
+<h3>Mochi Belga - R$ 5,90</h3>
+</div>
+<div class="homeproduto1">
+<img src="images/mochi_belga.jpg" alt="Mochi">
+<h3>Mochi Belga - R$ 5,90</h3>
+</div>
+<div class="homeproduto1">
+<img src="images/mochi_belga.jpg" alt="Mochi">
+<h3>Mochi Belga - R$ 5,90</h3>
+</div>
